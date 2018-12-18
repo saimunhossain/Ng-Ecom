7209e5f74b7f82d443c892d9d0296a74979f3b4e
Saimun Hossain <saimonanam@gmail.com>
2018-12-19 00:38:37 +0600

    deploy the project to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBaBZz29irgstlDuTnUE3NzYka4PF94fgk",
+    authDomain: "ng-econ.firebaseapp.com",
+    projectId: "ng-econ",
+    storageBucket: "project.appspot.com"
+  }
 };
